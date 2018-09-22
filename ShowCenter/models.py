439d20e99fe318ab107011764e548bcb044a1385
colacadstink <benharris5@gmail.com>
2018-09-21 23:08:06 -0400

    Did some initial model setup for ShowCenter

--- ShowCenter/models.py
+++ ShowCenter/models.py
@@ -1,5 +1,27 @@
 from django.db import models
+from jsonfield import JSONField
 
-# Create your models here.
-class Greeting(models.Model):
-    when = models.DateTimeField('date created', auto_now_add=True)
+
+class TaskDescription(models.Model):
+    name = models.CharField(max_length=255)
+    type = models.CharField(max_length=255)
+    options = JSONField()
+
+
+class Task(models.Model):
+    description = models.ForeignKey(TaskDescription, null=True, on_delete=models.SET_NULL)
+    settings = JSONField()
+    completed = models.BooleanField(default=False)
+
+
+class Show(models.Model):
+    name = models.CharField(max_length=255)
+    tasks = models.ManyToManyField(Task)
+    settings = JSONField()
+
+
+class Episode(models.Model):
+    name = models.CharField(max_length=255)
+    show = models.ForeignKey(Show, null=True, on_delete=models.SET_NULL)
+    tasks = models.ManyToManyField(Task)
+    settings = JSONField()
