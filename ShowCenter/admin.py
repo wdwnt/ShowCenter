4193d478a3a78e40d18c56f3b9396201af1a2b1c
colacadstink <benharris5@gmail.com>
2018-09-21 23:08:06 -0400

    Did some initial model setup for ShowCenter

--- ShowCenter/admin.py
+++ ShowCenter/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from ShowCenter import models
+
+admin.site.register(models.TaskDescription)
+admin.site.register(models.Task)
+admin.site.register(models.Show)
+admin.site.register(models.Episode)
