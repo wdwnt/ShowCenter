36394256ce2668da7937bb8cd084a5a4bd49b603
colacadstink <benharris5@gmail.com>
2018-09-21 20:28:45 -0400

    Imported ParksCenter as a Django app, which will make integrating it with ShowCenter far easier.

---
+++ ParksCenter/static/app.js
@@ -0,0 +1,26 @@
+angular.module('parkscenter', ['ngRoute', 'ngMaterial'])
+.config(function($routeProvider, $mdThemingProvider){
+	$routeProvider
+        .when('/pc/edit', {
+            template: '<hud-editor></hud-editor>'
+        })
+    	.when('/pc/hud', {
+    		template: '<hud-main></hud-main>'
+    	})
+		.when('/vs/edit', {
+			template: '<vs-hud-editor></vs-hud-editor>'
+		})
+		.when('/vs/hud', {
+			template: '<vs-hud-main></vs-hud-main>'
+		})
+    	.otherwise({
+    		templateUrl: STATIC_BASE+'templates/landing.template.html'
+    	});
+	
+	$mdThemingProvider.theme('default')
+		.primaryPalette('blue')
+		.accentPalette('light-blue');
+}).run(function($rootScope, $location){
+	$rootScope.$location = $location;
+	$rootScope.STATIC_BASE = STATIC_BASE;
+});
---
+++ ParksCenter/static/components/pc/hud-editor.component.js
@@ -0,0 +1,65 @@
+angular.module('parkscenter')
+.component('hudEditor', {
+	templateUrl: STATIC_BASE+'templates/pc/hud-editor.template.html',
+	controller: function($scope, $rootScope, $location){
+		var ctrl = this;
+		
+		ctrl.$onInit = function(){
+            if(!$rootScope.showData){
+            	if(sessionStorage.getItem("lastPCGame")){
+					$rootScope.showData = JSON.parse(sessionStorage.getItem("lastPCGame"));
+				}else{
+					var item = {
+						shortName: "",
+						longName: "",
+						thumbnail: STATIC_BASE+"images/wdwntLogo.png",
+						duration: 210
+					};
+					$rootScope.showData = [angular.copy(item), angular.copy(item), angular.copy(item),
+						angular.copy(item), angular.copy(item), angular.copy(item), angular.copy(item)];
+				}
+            }
+			
+			ctrl.curIndex=0;
+			
+			ctrl.imageList = [STATIC_BASE+'images/wdwntLogo.png'];
+			angular.forEach($rootScope.showData, function(item){
+				ctrl.imageList.push(item.thumbnail);
+			});
+
+			$scope.$watch(function(){
+				return angular.toJson($rootScope.showData);
+			}, function(newVal){
+				if(newVal){
+					sessionStorage.setItem("lastPCGame", newVal);
+				}
+			})
+		};
+		
+		ctrl.getItemState = function(index){
+			if(index<ctrl.curIndex){
+				return "past";
+			}else if(index>ctrl.curIndex){
+				return "future";
+			}else{
+				return "present";
+			}
+		};
+
+		ctrl.loadJSON = function(){
+			$rootScope.showData = JSON.parse(ctrl.jsonInput);
+		};
+
+		ctrl.gotoShow = function(){
+			$location.url("/pc/hud");
+		};
+
+		ctrl.getShowDuration = function(){
+			var total = 0;
+			angular.forEach($rootScope.showData, function(e){
+				total += e.duration;
+			});
+			return Math.floor(total/60)+":"+(total%60<10?'0':'')+(total%60);
+		}
+	}
+});
