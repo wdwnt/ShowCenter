3bd65bdaa05636f7e318004ca1ce8d4da30b52df
Michael V. Battista <github@mvbattista.com>
2018-10-02 00:59:08 -0400

    Clean up and modularize URLs
    Let's start by knowing where we're going.

--- WDWNTShowCenter/urls.py
+++ WDWNTShowCenter/urls.py
@@ -2,20 +2,18 @@ from django.conf.urls import include, url
 from django.urls import path, re_path
 
 from django.contrib import admin
-from django.views.generic import TemplateView
 
 admin.autodiscover()
 
-import ShowCenter.views
-
-from ParksCenter.views import ParksCenterTemplateView
+import ShowCenter.urls
+import ParksCenter.urls
 
 # Examples:
 # url(r'^$', 'WDWNTShowCenter.views.home', name='home'),
 # url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
-    url(r'^$', ShowCenter.views.index, name='index'),
-    path('parkscenter/', TemplateView.as_view(template_name='ParksCenter/index.html')),
+    path('', include(ShowCenter.urls)),
+    path('parkscenter/', include(ParksCenter.urls)),
     path('admin/', admin.site.urls),
 ]
