991dff7f79f3f0b206a1060d609a478b1eb4a474
colacadstink <benharris5@gmail.com>
2018-10-06 22:22:22 -0400

    Removed unnecessary requests request.

--- ShowCenter/views.py
+++ ShowCenter/views.py
@@ -1,11 +1,6 @@
 from django.http import HttpResponse
 
-import requests
 
 # Create your views here.
 def index(request):
-    # return HttpResponse('Hello from Python!')
-    # return render(request, 'index.html')
-    r = requests.get('http://httpbin.org/status/418')
-    return HttpResponse('<pre>' + r.text + '</pre>')
-
+    return HttpResponse('<pre>Hello world!</pre>')
