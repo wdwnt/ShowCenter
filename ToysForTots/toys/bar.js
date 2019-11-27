6239cdf24ae8bfd32ad2a83d065a46a7071a8ae5
Ben Harris <benharris5@gmail.com>
2019-11-27 00:08:40 -0500

    Initial bar setup

--- ToysForTots/toys/bar.js
+++ ToysForTots/toys/bar.js
@@ -1,36 +1,71 @@
-CountDownTimer('11/29/2019 9:00 PM', 'countdown');
-CountDownTimer('12/1/2019 11:00 PM', 'newcountdown');
+angular.module('toysBar', ['ngMaterial'])
+.component('progressBar', {
+	templateUrl: 'bar.html',
+	controller: function($http, $timeout){
+		const ctrl = this;
 
-function CountDownTimer(dt, id) {
-	var end = new Date(dt);
+		const DATA_URL = "https://internalapi-events.salsalabs.org/v1/public_event/fundraisers/4bf7cba7-ca2c-47a4-9dc5-1929a0e77b33?eventId=9e5c23ba-f2ea-4ab2-9f47-3bbfff085e45";
+		const END_DATE = new Date("2019-12-01T23:00:00-05:00");
+		const REFRESH_RATE = 30*1000; //ms
 
-	var _second = 1000;
-	var _minute = _second * 60;
-	var _hour = _minute * 60;
-	var _day = _hour * 24;
-	var timer;
+		const _second = 1000; //ms
+		const _minute = _second * 60;
+		const _hour = _minute * 60;
+		const _day = _hour * 24;
 
-	function showRemaining() {
-		var now = new Date();
-		var distance = end - now;
-		if (distance < 0) {
+		ctrl.refresh = function(){
+			$http.get(DATA_URL).then(function (resp) {
+				const fundraiser = resp.data.payload.fundraiser;
+				ctrl.progressDollars = fundraiser.amountRaised;
+				ctrl.donations = fundraiser.donors;
+				ctrl.goal = fundraiser.goalAmount;
 
-			clearInterval(timer);
-			document.getElementById(id).innerHTML = '0';
+				ctrl.progressPercent = Math.floor(1000 * ctrl.progressDollars / ctrl.goal)/10;
 
-			return;
-		}
-		var days = Math.floor(distance / _day);
-		var hours = Math.floor((distance % _day) / _hour);
-		var minutes = Math.floor((distance % _hour) / _minute);
-		var seconds = Math.floor((distance % _minute) / _second);
+				let now = new Date();
+				let distance = END_DATE - now;
+				if (distance < 0) {
+					ctrl.timeLeft = {
+						number: "No",
+						units: "Time"
+					};
+				}else{
+					let days = Math.floor(distance / _day);
+					let hours = Math.floor((distance % _day) / _hour);
+					let minutes = Math.floor((distance % _hour) / _minute);
+					let seconds = Math.floor((distance % _minute) / _second);
 
-		document.getElementById(id).innerHTML = days /*+ ' days'*/;
-		//document.getElementById(id).innerHTML += hours + 'hrs ';
-		//document.getElementById(id).innerHTML += minutes + 'mins ';
-		//document.getElementById(id).innerHTML += seconds + 'secs';
+					if(days){
+						ctrl.timeLeft = {
+							number: days,
+							units: "Days"
+						};
+					}else if(hours){
+						ctrl.timeLeft = {
+							number: hours,
+							units: "Hours"
+						};
+					}else if(minutes){
+						ctrl.timeLeft = {
+							number: minutes,
+							units: "Minutes"
+						};
+					}else{
+						ctrl.timeLeft = {
+							number: seconds,
+							units: "Seconds"
+						};
+					}
+				}
+			});
+		};
+
+		ctrl.$onInit = function() {
+			function refreshLoop(){
+				ctrl.refresh();
+				$timeout(refreshLoop, REFRESH_RATE);
+			}
+			refreshLoop();
+		};
 	}
-
-	timer = setInterval(showRemaining, 1000);
-}
-
+});
