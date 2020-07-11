349c7b4e56228658fd559cf4a36ee6e826ecd1c7
Ben Harris <benharris5@gmail.com>
2020-07-10 20:11:50 -0400

    Finalized everything, and integrated with vMix

--- SuperObscureGameShow/sogs/components/sogs-main.js
+++ SuperObscureGameShow/sogs/components/sogs-main.js
@@ -1,21 +1,25 @@
 angular.module('sogs')
 .component('sogsMain', {
 	templateUrl: 'templates/sogs-main.html',
-	controller: function ($scope, $rootScope, $location, $timeout, $filter, $q, vMix) {
+	controller: function ($scope, $location, vMix, $timeout, $filter, $q) {
 		const ctrl = this;
+
+		const VMIX_POINTS_BAR_NAME = "Points bar";
 
 		ctrl.data = [];
 		ctrl.curItem = -1;
 
 		ctrl.reveal = function(item){
-			if(!item.revealed){
+			if(!item.startedRevealing && !item.revealed){
+				item.startedRevealing = true;
 				ctrl.go(item.value, item);
 			}
 		}
 
 		ctrl.go = function(amount, item){
-			console.log("go to "+amount);
-			$scope.$broadcast("play", amount, item);
+			vMix().overlay(VMIX_POINTS_BAR_NAME, 1)
+				.wait(500)
+				.then(function(){$scope.$broadcast("play", amount, item)});
 		}
 
 		ctrl.reset = function(){
@@ -24,12 +28,22 @@ angular.module('sogs')
 
 		ctrl.load = function(gameJson){
 			ctrl.data = JSON.parse(gameJson);
+			$location.search("json", JSON.stringify(ctrl.data));
 		}
 
 		$scope.$on("finished", function(event, item){
+			vMix().overlay(VMIX_POINTS_BAR_NAME, 1)
+				.wait(2000)
+				.then(function(){ctrl.reset()});
 			if(item){
 				item.revealed =  true;
 			}
 		});
+
+		ctrl.$onInit = function(){
+			if($location.search()["json"]){
+				ctrl.load($location.search()["json"]);
+			}
+		};
 	}
 })
